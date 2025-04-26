ae0c84b5ecd02cecf9d370b553ac980950300ab8
Michael May <mskmay66@gmail.com>
2025-04-26 13:18:39 -0400

    added keneck lattice and rulkov lattice to __init__

--- src/cml/__init__.py
+++ src/cml/__init__.py
@@ -1,4 +1,6 @@
 from .cml import CoupledMapLattice
 from .viz import Visualization
+from .kaneko import KenekoLattice
+from .rulkov import RulkovLattice
 
-__all__ = ["CoupledMapLattice", "Visualization"]
+__all__ = ["CoupledMapLattice", "Visualization", "KenekoLattice", "RulkovLattice"]
